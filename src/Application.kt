2f631116920a7f3a1ed2ee8c2e989b76bb328b53
okaryo <okaryotr@gmail.com>
2021-07-28 22:30:19 +0900

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+//fun main() {
+//    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+//        configureRouting()
+//    }.start(wait = true)
+//}
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
